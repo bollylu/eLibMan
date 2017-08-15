3687caef9dfa96ac4ab1137ee72dc38915d3e2c9
Luc Bolly <lbolly@hotmail.com>
2017-08-15 22:56:06 +0200

    Simplified rewrite

--- MakeCbz/Program.cs
+++ MakeCbz/Program.cs
@@ -12,6 +12,14 @@ using BLTools.Debugging;
 
 namespace MakeCbz {
   class Program {
+
+    static List<string> ExcludedExtensions = new List<string> { ".cbz", ".cbr", ".pdf" };
+    enum EProcessStatus {
+      Ok,
+      Warning,
+      Error
+    }
+
     static void Main(string[] args) {
 
       TraceFactory.AddTraceConsole();
@@ -19,32 +27,15 @@ namespace MakeCbz {
 
       string SourceDir = Args.GetValue("sourcedir", "");
       Console.WriteLine($"Source directory = {SourceDir}");
-      InitTitle($"Processing {SourceDir}");
 
-      DirectoryInfo FirstLevelDir = new DirectoryInfo(SourceDir);
-      IEnumerable<DirectoryInfo> SubDirs = FirstLevelDir.EnumerateDirectories();
-
-      if ( SubDirs.Count() == 0 ) {
-        string CbzFilename = $"{SourceDir}.cbz";
-        ChangeTitle($"Building {CbzFilename}");
-        if ( !ProcessDirectory(FirstLevelDir, CbzFilename) ) {
-          ConsoleExtension.Pause();
-          Environment.Exit(1);
-        } else {
-          //ConsoleExtension.Pause();
-          Environment.Exit(0);
-        }
+      if ( string.IsNullOrWhiteSpace(SourceDir) || !Directory.Exists(SourceDir) ) {
+        Usage($"Missing source directory : directory is missing or access is denied");
       }
 
-      List<string> Errors = new List<string>();
-      SubDirs.AsParallel().ForAll((x) => {
-        string CbzFilename = $"{x.FullName}.cbz";
-        if ( !ProcessDirectory(x, CbzFilename) ) {
-          Errors.Add(CbzFilename);
-        }
-      });
+      InitTitle($"Processing {SourceDir}");
 
-      if ( Errors.Count > 0 ) {
+      EProcessStatus ProcessStatus = ProcessDirectory(new DirectoryInfo(SourceDir));
+      if ( ProcessStatus != EProcessStatus.Ok ) {
         ConsoleExtension.Pause();
         Environment.Exit(1);
       }
@@ -54,50 +45,67 @@ namespace MakeCbz {
 
     }
 
-    static private bool ProcessDirectory(DirectoryInfo directory, string cbzFilename) {
-      StringBuilder ActionLog = new StringBuilder();
-      ActionLog.AppendLine($"Processing {directory.FullName}");
+
+
+    static private EProcessStatus ProcessDirectory(DirectoryInfo directory) {
 
       try {
-        if ( directory == null || !Directory.Exists(directory.FullName) ) {
-          ActionLog.AppendLine($"  => Unable to create {cbzFilename} file : source directory is missing or access is denied");
-          return false;
-        }
+        Trace.WriteLine($"Processing {directory.FullName}");
+        Trace.Indent();
 
-        IEnumerable<DirectoryInfo> SubDirs = directory.EnumerateDirectories("*", SearchOption.TopDirectoryOnly);
+        string CbzFilename = $"{directory.FullName}.cbz";
 
-        if ( SubDirs.Count() > 0 ) {
-          Trace.WriteLine($"  => ## Found {SubDirs.Count()} sub-directories, digging first ##");
-          foreach ( DirectoryInfo DirectoryItem in SubDirs ) {
+        Trace.WriteLine($"Attempt to create {CbzFilename}");
+        try {
+          if ( directory == null || !Directory.Exists(directory.FullName) ) {
+            Trace.WriteLine($"  => Unable to create {CbzFilename} file : source directory is missing or access is denied", Severity.Error);
+            return EProcessStatus.Error;
+          }
 
-            if ( !ProcessDirectory(DirectoryItem, $"{DirectoryItem.FullName}.cbz") ) {
-              return false;
+          DirectoryInfo[] SubDirs = directory.GetDirectories("*", SearchOption.TopDirectoryOnly);
+
+          if ( SubDirs.Count() > 0 ) {
+            Trace.WriteLine($"  => ## Found {SubDirs.Count()} sub-directories ##");
+            EProcessStatus GlobalProcessStatus = EProcessStatus.Ok;
+            foreach ( DirectoryInfo DirectoryItem in SubDirs ) {
+              EProcessStatus ProcessStatus = ProcessDirectory(DirectoryItem);
+              if ( ProcessStatus != EProcessStatus.Ok ) {
+                GlobalProcessStatus = ProcessStatus;
+              }
+            }
+            return GlobalProcessStatus;
+
+          } else {
+
+            IEnumerable<FileInfo> ExcludedFiles = directory.GetFiles("*", SearchOption.TopDirectoryOnly).Where(x => ExcludedExtensions.Contains(x.Extension.ToLower()));
+            if ( ExcludedFiles.Count() > 0 ) {
+              Trace.WriteLine($"  => *** Skipping {CbzFilename} : Found excluded files in folder ***", Severity.Warning);
+              foreach ( string FileItem in ExcludedFiles.Select(x => x.Name) ) {
+                Trace.WriteLine($"    {FileItem}");
+              }
+              return EProcessStatus.Warning;
             }
 
+            if ( File.Exists(CbzFilename) ) {
+              Trace.WriteLine($"  => Unable to create {CbzFilename} : file already exists", Severity.Error);
+              return EProcessStatus.Error;
+            }
+
+            ChangeTitle($"Building {CbzFilename}");
+            Trace.WriteLine($"  => Generate {CbzFilename}");
+            ZipFile.CreateFromDirectory(directory.FullName, CbzFilename, CompressionLevel.Fastest, false);
+            directory.Delete(true);
+            ChangeTitle();
+            return EProcessStatus.Ok;
+
           }
+
+        } catch ( Exception ex ) {
+          Trace.WriteLine($"  => Error while processing : {ex.Message}", Severity.Error);
+          return EProcessStatus.Error;
         }
-
-        if ( directory.EnumerateFiles("*", SearchOption.TopDirectoryOnly).Where(x => x.Extension.ToLower() != ".cbz").Count() > 0 ) {
-
-          if ( File.Exists(cbzFilename) ) {
-            ActionLog.AppendLine($"  => Unable to create {cbzFilename} : file already exists");
-            return false;
-          }
-
-          ChangeTitle($"Building {cbzFilename}");
-          ActionLog.AppendLine($"  => Will generate {cbzFilename}");
-          ZipFile.CreateFromDirectory(directory.FullName, cbzFilename, CompressionLevel.Fastest, false);
-          directory.Delete(true);
-          ChangeTitle();
-          return true;
-        }
-
-        return true;
-      } catch ( Exception ex ) {
-        ActionLog.AppendLine($"  => Error while processing : {ex.Message}");
-        return false;
       } finally {
-        Trace.WriteLine(ActionLog.Trim('\r', '\n'));
+        Trace.Unindent();
       }
     }
 
@@ -109,6 +117,12 @@ namespace MakeCbz {
     static void ChangeTitle(string suffixTitle = "") {
       string CombinedTitle = string.IsNullOrWhiteSpace(suffixTitle) ? BaseTitle : $"{BaseTitle} - {suffixTitle}";
       Console.Title = CombinedTitle;
+    }
+
+    private static void Usage(string message = "") {
+      Console.WriteLine("MakeCbz v2.0");
+      Console.WriteLine("Usage: MakeCbz /SourceDir=<folder to convert>");
+      Environment.Exit(2);
     }
   }
 }
